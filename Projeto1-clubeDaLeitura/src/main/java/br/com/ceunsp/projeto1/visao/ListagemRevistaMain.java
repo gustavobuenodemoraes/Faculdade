720a1116b065801c4ead77bfb4371d78038b315d
Gustavo Bueno <gustavobuenodemoraes@yahoo.com.br>
2016-05-25 14:16:40 -0300

    Coloca a tela lisa cadastro e caixa

---
+++ Projeto1-clubeDaLeitura/src/main/java/br/com/ceunsp/projeto1/visao/ListagemRevistaMain.java
@@ -0,0 +1,32 @@
+package br.com.ceunsp.projeto1.visao;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class ListagemRevistaMain extends Application{
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("fxml/Listagem_revista.fxml"));
+			
+			Scene scene = new Scene(root,500,350);
+			scene.getStylesheets().add(getClass().getResource("css/application.css").toExternalForm());
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("imagens/listaRevistared.png")));//imagem
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Listagem de Revistas");
+			primaryStage.setMaximized(false);
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
