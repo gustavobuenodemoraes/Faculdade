1c60af29a851b868163b0b7d0149d6bd09ca2fa3
Gustavo Bueno <gustavobuenodemoraes@yahoo.com.br>
2016-05-27 21:11:33 -0300

    Atualização dos icones

--- Projeto1-clubeDaLeitura/src/main/java/br/com/ceunsp/projeto1/visao/DevolucaoMain.java
+++ Projeto1-clubeDaLeitura/src/main/java/br/com/ceunsp/projeto1/visao/DevolucaoMain.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class DevolucaoMain extends Application{
@@ -14,8 +15,10 @@ public class DevolucaoMain extends Application{
 			
 			Scene scene = new Scene(root, 420, 250);
 			scene.getStylesheets().add(getClass().getResource("css/application.css").toExternalForm());
+			
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("imagens/devolucaored.png")));//imagem
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Cadastro de revistas");
+			primaryStage.setTitle("Devolucao de revistas");
 			primaryStage.setMaximized(false);
 			primaryStage.setResizable(false);
 			primaryStage.show();
